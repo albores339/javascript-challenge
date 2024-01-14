6487905bb5e48145f5d61ed64566bf54925b3f58
Sonia Aguirre <arembiam@gmail.com>
2024-01-14 14:18:25 -0600

    tooltips on buttons

---
+++ newPost/js/createpost.js
@@ -0,0 +1,7 @@
+/*TOOLTIPS*/
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
